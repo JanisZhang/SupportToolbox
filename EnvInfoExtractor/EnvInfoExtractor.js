509328a8ad115aee594bf55ef40cd403e3b22cd8
JiadiZhang <janiszhang.se@gmail.com>
2025-03-24 10:53:04 +0800

    Add copyright notice and MIT license header to all code files

--- EnvInfoExtractor/EnvInfoExtractor.js
+++ EnvInfoExtractor/EnvInfoExtractor.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025 JanisZhang
+ *
+ * This file is licensed under the MIT License.
+ * See the LICENSE file in the project root for full license information.
+ */
+
+
 const style = document.createElement("style");
 style.innerHTML = `
   .disabled {
--- URL_converter/convertedUrl.js
+++ URL_converter/convertedUrl.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025 JanisZhang
+ *
+ * This file is licensed under the MIT License.
+ * See the LICENSE file in the project root for full license information.
+ */
+
+
 console.log("This is a popup!");
 
 // 创建并插入 .disabled 样式到页面中
